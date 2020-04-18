2cee814a9a040cc6c002af18aad3078d3ad3e4a5
SmintCodes <46701284+SmintGaming@users.noreply.github.com>
2020-04-18 11:42:50 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+var cacheName = 'Reddit TTS Generator';
+var filesToCache = [
+  '/RedditTTSGenerator/',
+  '/RedditTTSGenerator/index.html',
+  '/RedditTTSGenerator/css/style.css',
+  '/RedditTTSGenerator/js/reddittts.js',
+  '/RedditTTSGenerator/css/404.css''
+  '/RedditTTSGenerator/404.html'    
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('installing cache : ' + cacheName)
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
